0995ab6968a42a69b702368a53c3f9413ec81dba
Wagner Duarte <46401074+WagnerofDuarte@users.noreply.github.com>
2023-10-10 16:42:21 -0300

    t

---
+++ Alura_Viagens/Controllers/HomeCollectionController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeCollectionController.swift
+//  Alura_Viagens
+//
+//  Created by dtiDigital on 10/10/23.
+//
+
+import UIKit
+
+class HomeCollectionController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
